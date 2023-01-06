ddf75e0967b6b06160c1217e05ed23c60417998d
miguelcintas <mcintas.c1@gmail.com>
2023-01-06 17:28:48 +0100

    main

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ const Home = ({ products, bannerData }) => (
   <div>
     <HeroBanner heroBanner={bannerData.length && bannerData[0]}  />
     <div className="products-heading">
-      <h2>Best perfurms</h2>
+      <h2>Best perfurms of sevilla</h2>
       <p>speaker There are many variations passages</p>
     </div>
 
