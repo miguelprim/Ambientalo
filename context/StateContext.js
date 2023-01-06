2440e726e1d928a907dcbe3fbc22c2a9a1c8eda3
miguelcintas <mcintas.c1@gmail.com>
2023-01-06 16:59:42 +0100

    first commit

---
+++ context/StateContext.js
@@ -0,0 +1,184 @@
+import React, { createContext, useContext, useState, useEffect } from 'react';
+import { toast } from 'react-hot-toast';
+
+const Context = createContext();
+
+
+
+export const StateContext = ({ children }) => {
+  const initialCart = [];
+  const initialQuantity = 0;
+  const initialPrice = 0;
+  
+  const [showCart, setShowCart] = useState(false);
+  const [cartItems, setCartItems] = useState(initialCart);
+  const [totalPrice, setTotalPrice] = useState(0);
+  const [totalQuantities, setTotalQuantities] = useState(initialQuantity);
+  const [qty, setQty] = useState(1);
+  
+  
+  
+
+  //
+  /** This will persist the cart items **/
+
+  useEffect(() => {
+     const cartData = JSON.parse(localStorage.getItem('cart'));
+     if (cartData) {
+        setCartItems(cartData);
+     }
+  }, []);
+
+  useEffect(() => {
+     if (cartItems !== initialCart) {
+        localStorage.setItem('cart', JSON.stringify(cartItems));
+     }
+  }, [cartItems]);
+
+  /** This will persist the quantity **/
+
+  useEffect(() => {
+     const cartQuantity = JSON.parse(localStorage.getItem('quantity'));
+     if (cartQuantity) {
+        setTotalQuantities(cartQuantity);
+     }
+  }, []);
+
+  useEffect(() => {
+     if (totalQuantities !== initialQuantity) {
+        localStorage.setItem('quantity', JSON.stringify(totalQuantities));
+     }
+  }, [totalQuantities]);
+
+  // price
+
+  useEffect(() => {
+    const cartPrice = JSON.parse(localStorage.getItem('price'));
+    if (cartPrice) {
+       setTotalPrice(cartPrice);
+    }
+ }, []);
+
+ useEffect(() => {
+    if (totalPrice !== initialPrice) {
+       localStorage.setItem('price', JSON.stringify(totalPrice));
+    }
+ }, [totalPrice]);
+
+ //
+
+  let foundProduct;
+  let index;
+
+  const onAdd = (product, quantity) => {
+    const checkProductInCart = cartItems.find((item) => item._id === product._id);
+    
+    setTotalPrice((prevTotalPrice) => prevTotalPrice + product.price * quantity);
+    setTotalQuantities((prevTotalQuantities) => prevTotalQuantities + quantity);
+    
+    if(checkProductInCart) {
+      const updatedCartItems = cartItems.map((cartProduct) => {
+        if(cartProduct._id === product._id) return {
+          ...cartProduct,
+          quantity: cartProduct.quantity + quantity
+        }
+        if ([cartProduct._id === product._id]) return {
+          ...cartProduct,
+        }
+      })
+
+      setCartItems(updatedCartItems);
+    } else {
+      product.quantity = quantity;
+      
+      setCartItems([...cartItems, { ...product }]);
+    }
+
+    toast.success(`${qty} ${product.name} added to the cart.`);
+  } 
+
+  const onRemove = (product) => {
+    foundProduct = cartItems.find((item) => item._id === product._id);
+    const newCartItems = cartItems.filter((item) => item._id !== product._id);
+
+    setTotalPrice((prevTotalPrice) => prevTotalPrice -foundProduct.price * foundProduct.quantity);
+    setTotalQuantities(prevTotalQuantities => prevTotalQuantities - foundProduct.quantity);
+    setCartItems(newCartItems);
+  }
+
+  const toggleCartItemQuanitity = (id, value) => {
+    foundProduct = cartItems.find((item) =>
+        item._id === id)
+    index = cartItems.findIndex((product) =>
+        product._id === id);
+
+    if (value === 'inc') {
+        setCartItems(
+            cartItems.map((item) => {
+                if (item._id === id) {
+                    return {
+                        ...item,
+                        quantity: item.quantity + 1
+                    };
+                }
+                return item;
+            }),
+        );
+        setTotalPrice((prevTotalPrice) =>
+            prevTotalPrice + foundProduct.price);
+        setTotalQuantities((prevTotalQuanties) =>
+            prevTotalQuanties + 1);
+    } else if (value === 'dec')
+        if (foundProduct.quantity > 1) {
+            setCartItems(
+                cartItems.map((item) => {
+                    if (item._id === id) {
+                        return { ...item, quantity: item.quantity - 1 };
+                    }
+                    return item;
+                }),
+            );
+            setTotalPrice((prevTotalPrice) => prevTotalPrice - foundProduct.price);
+            setTotalQuantities((prevTotalQuantities) => prevTotalQuantities - 1);
+
+        }
+}
+
+  const incQty = () => {
+    setQty((prevQty) => prevQty + 1);
+  }
+
+  const decQty = () => {
+    setQty((prevQty) => {
+      if(prevQty - 1 < 1) return 1;
+     
+      return prevQty - 1;
+    });
+  }
+
+  return (
+    <Context.Provider
+      value={{
+        showCart,
+        setShowCart,
+        cartItems,
+        totalPrice,
+        totalQuantities,
+        qty,
+        incQty,
+        decQty,
+        onAdd,
+        toggleCartItemQuanitity,
+        onRemove,
+        setCartItems,
+        setTotalPrice,
+        setTotalQuantities,
+        setQty 
+      }}
+    >
+      {children}
+    </Context.Provider>
+  )
+}
+
+export const useStateContext = () => useContext(Context);
---
+++ pages/api/stripe.js
@@ -0,0 +1,56 @@
+import Stripe from 'stripe';
+
+const stripe = new Stripe(process.env.NEXT_PUBLIC_STRIPE_SECRET_KEY);
+
+export default async function handler(req, res) {
+  if (req.method === 'POST') {
+    try {
+      const params = {
+        submit_type: 'pay',
+        mode: 'payment',
+        payment_method_types: ['card'],
+        
+        shipping_options: [
+          { shipping_rate: 'shr_1MMAB2KZZGjzV8PH1WmkJDXr' },
+        ],
+        billing_address_collection: 'required',
+        shipping_address_collection: {
+          allowed_countries: ['ES', 'DE','IT','PT','FR','GB','IE','CH','AT','NO','SE'],
+        },
+
+        line_items: req.body.map((item) => {
+          const img = item.image[0].asset._ref;
+          const newImage = img.replace('image-', 'https://cdn.sanity.io/images/24tjno36/production/').replace('-webp', '.webp');
+
+          return {
+            price_data: { 
+              currency: 'eur',
+              product_data: { 
+                name: item.name,
+                images: [newImage],
+              },
+              unit_amount: item.price * 100,
+            },
+            adjustable_quantity: {
+              enabled:true,
+              minimum: 1,
+            },
+            quantity: item.quantity
+          }
+        }),
+        success_url: `${req.headers.origin}/success`,
+        cancel_url: `${req.headers.origin}/`,
+      }
+
+      // Create Checkout Sessions from body params.
+      const session = await stripe.checkout.sessions.create(params);
+
+      res.status(200).json(session);
+    } catch (err) {
+      res.status(err.statusCode || 500).json(err.message);
+    }
+  } else {
+    res.setHeader('Allow', 'POST');
+    res.status(405).end('Method Not Allowed');
+  }
+}
