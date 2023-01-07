4ec42371c6714ffe76a0c4e727fcc2f3bb7cca30
miguelcintas <mcintas.c1@gmail.com>
2023-01-07 19:30:45 +0100

    er

--- sanity_ecommerce/schemas/schema.js
+++ sanity_ecommerce/schemas/schema.js
@@ -3,11 +3,10 @@ import schemaTypes from 'all:part:@sanity/base/schema-type';
 
 import product from './product';
 import banner from './banner';
-import logo from './logo';
 
 export default createSchema({
   name: 'default',
-  types: schemaTypes.concat([ product, banner, logo ]),
+  types: schemaTypes.concat([ product, banner ]),
 
  
     
