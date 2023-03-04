7d77582875ae94d9f215af6061796bd8812ec11e
miguelcintas <mcintas.c1@gmail.com>
2023-03-04 21:58:56 +0100

    234

--- pages/api/stripe.js
+++ pages/api/stripe.js
@@ -27,7 +27,7 @@ export default async function handler(req, res) {
               currency: 'eur',
               product_data: { 
                 name: item.name,
-                images: [newImage],
+                images: [],
               },
               unit_amount: item.price * 100,
             },
