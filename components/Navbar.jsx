1149507bb4f8f8b51d22df77ac1d08a5f007b9c4
miguelcintas <mcintas.c1@gmail.com>
2024-01-21 17:50:37 +0100

    sip

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -12,7 +12,7 @@ const Navbar = () => {
     <div className="navbar-container">
       
       <Link class="navbar-logo" href="/">
-      <img src="/logo/logo.webp" ></img></Link>        
+      <img src="/logo/ambientalo3.png" ></img></Link>        
 
       <p className="contact">
       <Link href="/contact">CONTACT</Link>
